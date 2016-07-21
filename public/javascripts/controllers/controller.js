a02cc5b1926e3487d2dea15a59cc83be647de2b6
Kirsten <kirstenwedde@gmail.com>
2016-07-20 21:40:07 -0600

    instantiate pirates module

---
+++ public/javascripts/controllers/controller.js
@@ -0,0 +1,4 @@
+app.controller('PiratesController', function($scope){
+  $scope.view = {};
+  $scope.view.value = 4; 
+});
---
+++ server.js
@@ -0,0 +1,2 @@
+var express = require('express');
+express().use(express.static(__dirname)).listen(3000); 
